04a821d7393e0327ff95426ace76fd5030b352b6
chuanzong.lcz <chuanzong.lcz@alibaba-inc.com>
2018-07-06 00:13:17 +0800

    feat：翻页器中文化

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,31 @@ import { AppComponent } from './app.component';
 import { SearchBoxModule } from "./search-box/search-box.module";
 import { AmusementParkModule } from "./amusement-park/amusement-park.module";
 
+import { MatPaginatorIntl } from '@angular/material';
+
+class MatPaginatorChinese extends MatPaginatorIntl {
+  itemsPerPageLabel = '当前页数';
+  firstPageLabel = '第一页';
+  lastPageLabel = '最后一页';
+  nextPageLabel = '下一页';
+  previousPageLabel = '上一页';
+
+  getRangeLabel = function (page, pageSize, length) {
+    if (length === 0 || pageSize === 0) {
+      return '0 of ' + length;
+    }
+
+    length = Math.max(length, 0);
+
+    const startIndex = page * pageSize;
+    const endIndex = startIndex < length ?
+      Math.min(startIndex + pageSize, length) :
+      startIndex + pageSize;
+
+    return '第 ' + (startIndex + 1) + ' - ' + endIndex + ' 条，共 ' + length + ' 条';
+  };
+}
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +43,9 @@ import { AmusementParkModule } from "./amusement-park/amusement-park.module";
     SearchBoxModule,
     AmusementParkModule,
   ],
-  providers: [],
+  providers: [
+    { provide: MatPaginatorIntl, useClass: MatPaginatorChinese }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
