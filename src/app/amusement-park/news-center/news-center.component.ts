18b6fa22b8608e7b2ed1a77748fec99ae6b50a3a
chuanzong.lcz <chuanzong.lcz@alibaba-inc.com>
2018-07-08 01:26:39 +0800

    feat: 增加 ted 视频

--- src/app/amusement-park/news-center/news-center.component.ts
+++ src/app/amusement-park/news-center/news-center.component.ts
@@ -2,17 +2,54 @@
  * 新闻中心
  */
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+interface ITed {
+  title: string,
+  url: string,
+  img: {
+    src: string,
+  }
+}
+
+interface IRawTed {
+  pageurl: string,
+  title: string,
+  imgpath: string,
+}
+
+interface ITedResponse {
+  data: IRawTed[]
+}
 
 @Component({
   selector: 'app-news-center',
   templateUrl: './news-center.component.html',
-  styleUrls: ['./news-center.component.css']
+  styleUrls: ['./news-center.component.scss']
 })
 export class NewsCenterComponent implements OnInit {
+  teds: Observable<ITed[]>;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
-  }
+    this.teds = this.http.jsonp('https://so.open.163.com/getMovieListByType.htm?pagesize=12', 'callback')
+      .pipe(
+        map(response => {
+          console.log('response:', response);
 
+          const items = (response as ITedResponse).data;
+
+          return items.map(({ pageurl, title, imgpath }) => ({
+            title,
+            url: pageurl,
+            img: {
+              src: `http://vimg1.ws.126.net${imgpath}.jpg`
+            }
+          }));
+        })
+      );
+  }
 }
