18dc2a9d611ca2b9902d1f7190e5d1491d7427fa
chuanzong.lcz <chuanzong.lcz@alibaba-inc.com>
2018-07-07 15:12:27 +0800

    feat(video-center): 翻页显示当前页

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { AppComponent } from './app.component';
+import { MatPaginatorIntl } from '@angular/material';
 
 import { SearchBoxModule } from "./search-box/search-box.module";
 import { AmusementParkModule } from "./amusement-park/amusement-park.module";
 
-import { MatPaginatorIntl } from '@angular/material';
+import { AppComponent } from './app.component';
 
 class MatPaginatorChinese extends MatPaginatorIntl {
   itemsPerPageLabel = '当前页数';
@@ -18,17 +17,12 @@ class MatPaginatorChinese extends MatPaginatorIntl {
 
   getRangeLabel = function (page, pageSize, length) {
     if (length === 0 || pageSize === 0) {
-      return '0 of ' + length;
+      return `0 of ${length}`;
     }
 
     length = Math.max(length, 0);
 
-    const startIndex = page * pageSize;
-    const endIndex = startIndex < length ?
-      Math.min(startIndex + pageSize, length) :
-      startIndex + pageSize;
-
-    return '第 ' + (startIndex + 1) + ' - ' + endIndex + ' 条，共 ' + length + ' 条';
+    return `第 ${page + 1} 页，共 ${length} 条`;
   };
 }
 
